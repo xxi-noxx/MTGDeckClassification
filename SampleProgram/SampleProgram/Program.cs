30c9020f3227e29124f89abc45dec1ed4c6fd504
xxi-noxx <handlkou@yahoo.co.jp>
2020-05-10 21:18:36 +0900

    細かいリファクタリング - 不要using削除 - コメント追加

--- SampleProgram/SampleProgram/Program.cs
+++ SampleProgram/SampleProgram/Program.cs
@@ -10,6 +10,8 @@ namespace SampleProgram
         {
             IClassification classification = new Classification();
 
+            // 今回はネットに乗っていたデッキのカード情報から手打ちで用意
+            // デッキ情報のインポートやカードデータの準備はアーキタイプ判定とは別の機能なので今回は実装無し。
             var deck = new TemurReclamation();
             var archetype = classification.GetArchetype(deck);
 
